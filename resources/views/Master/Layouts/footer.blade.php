02869132a5401df3564c883849ff4671d83620da
Feri <Feriiifirdauss350@gmail.com>
2025-01-09 13:18:05 +0700

    uasweb3

---
+++ resources/views/Master/Layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <div class="row align-items-center flex-row-reverse">
+            <div class="col-md-12 col-sm-12 text-center">
+                Copyright © {{date('Y')}} | Repost by <a href='https://stokcoding.com/' title='StokCoding.com' target='_blank'>StokCoding.com</a>
+
+            </div>
+        </div>
+    </div>
+</footer>
